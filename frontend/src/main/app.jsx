fc27c5fafe50cccbf542530dafbbbf39946b98bf
sirAndre1337 <andrelluis17@hotmail.com>
2022-05-28 15:28:26 -0300

    Create index.html, components App,Todo,About,Menu

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,18 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+import React from 'react'
+import About from '../about/about'
+import Menu from '../template/menu'
+import Todo from '../todo/todo'
+
+const App = () => {
+    return (
+        <div className='container'>
+            <Menu />
+            <Todo />
+            <About />
+        </div>
+    )
+}
+
+export default App;

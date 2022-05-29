2c35a026c2ea1a400acf13a6417393378f02bc0e
sirAndre1337 <andrelluis17@hotmail.com>
2022-05-28 21:26:50 -0300

    Components PageHeader,TodoForm and TodoList

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import PageHeader from "../template/pageHeader";
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name="Sobre" small="Eu"/>
+        <h2>Minha historia</h2>
+        <p>Comecei estudando c/c++ na faculdade, depois estudei o java fiz projetos usando JSP/JSF/Servlets/Thymeleaf/JDBC/Spring MVC/Spring Rest/Angular/React. Conheci React e estou estudando o mesmo.</p>
+        <h2>Missao</h2>
+        <p>Terminar react, estudar NextJS, e provalvemente comecar a estudar PHP.</p>
+        <h2>Contato:</h2>
+        <p><strong>Email</strong>: andrelluis17@hotmail.com</p>
+        <p><strong>Git</strong>: sirAndre1337</p>
     </div>
 )

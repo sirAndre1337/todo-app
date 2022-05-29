85c815a6d73f1cbb0c1517043b21b503e6434b5b
sirAndre1337 <andrelluis17@hotmail.com>
2022-05-28 22:10:59 -0300

    Event add,onChange integrate add todo with backend

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,14 +1,44 @@
-import React, {Component } from "react";
+import axios from "axios";
+import React, { Component } from "react";
 import PageHeader from "../template/pageHeader";
 import TodoForm from "./todoForm";
 import TodoList from "./todoList";
 
+const URL = 'http://localhost:3003/api/todos'
+
 export default class Todo extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: [] }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({
+            ...this.state , description : e.target.value
+        })
+    }
+
+    handleAdd() {
+        const description = this.state.description;
+        axios.post(URL, {description})
+            .then(resp => console.log('Funcionou'))
+    }
+
+    
+
     render() {
         return (
             <div>
-                <PageHeader name="Tarefas" small="Cadastro"/>
-                <TodoForm />
+                <PageHeader name="Tarefas" small="Cadastro" />
+                <TodoForm
+                    handleAdd={this.handleAdd}
+                    description={this.state.description}
+                    handleChange={this.handleChange}
+                />
                 <TodoList />
             </div>
         )
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Grid from "../template/grid";
 import IconButton from "../template/iconButton";
-import If from "../template/if";
 
 export default props => (
     <div>
         <div role='form' className="todoForm">
             <Grid cols="12 9 10">
-                <input className="form-control" placeholder="Adicione uma tarefa" id="description" />
+                <input
+                    className="form-control"
+                    placeholder="Adicione uma tarefa"
+                    id="description"
+                    value={props.description}
+                    onChange={props.handleChange}
+                />
             </Grid>
         </div>
         <Grid cols="12 3 2">
-            <IconButton style="primary" icon="plus" />
+            <IconButton style="primary" icon="plus" onClick={props.handleAdd} />
         </Grid>
     </div>
 )

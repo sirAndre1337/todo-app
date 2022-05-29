a8ea94a1f3e63eaa60581757bedd2ee0b6e88532
sirAndre1337 <andrelluis17@hotmail.com>
2022-05-28 21:49:55 -0300

    Components form, grid, iconButton and if

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Grid from "../template/grid";
+import IconButton from "../template/iconButton";
+import If from "../template/if";
 
 export default props => (
     <div>
-        <h1>Form</h1>
+        <div role='form' className="todoForm">
+            <Grid cols="12 9 10">
+                <input className="form-control" placeholder="Adicione uma tarefa" id="description" />
+            </Grid>
+        </div>
+        <Grid cols="12 3 2">
+            <IconButton style="primary" icon="plus" />
+        </Grid>
     </div>
 )
